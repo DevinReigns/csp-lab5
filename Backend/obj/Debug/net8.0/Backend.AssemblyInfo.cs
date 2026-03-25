4c9183fe875a118416b0c5fa914eecd5f28fff38
DevinReigns <devinreigns112@gmail.com>
2026-03-25 18:12:04 +0530

    add swagger package

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+830638ccb49c4af00e1e481c7d7698a5dbb7622d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f633ee7a8a462df56354819328e6f18d5bb4e2e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
